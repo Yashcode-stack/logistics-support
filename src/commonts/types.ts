4c940a75267056f48a277f8e9c478e2d432c27dd
Yashcode-stack <yash.dua@primathon.in>
2025-10-16 19:11:27 +0530

    New updated changes

--- src/commonts/types.ts
+++ src/commonts/types.ts
@@ -2,3 +2,16 @@ export interface ServiceabilityCheckType {
   originPincodes: string[];
   destinationPincodes: string[];
 }
+
+export interface Location {
+  _id?: string;
+  pincode?: number;
+  cityName?: string;
+  state?: string;
+  deliveryPartnerId?: string;
+}
+
+export interface Partner {
+  originLocations: Location[];
+  destinationLocations: Location[];
+}
--- src/serviceability/serviceability.service.ts
+++ src/serviceability/serviceability.service.ts
@@ -3,6 +3,7 @@ import { PartnerLocationsService } from "../partner-locations/partner-locations.
 import { DeliveryPartnersService } from "src/delivery-partners/delivery-partners.service";
 import { ZoneMappingsService } from "src/zone-mappings/zone-mappings.service";
 import { Types } from "mongoose";
+import { Partner } from "src/commonts/types";
 
 @Injectable()
 export class ServiceabilityService {
@@ -41,335 +42,280 @@ export class ServiceabilityService {
     return results;
   }
 
-  async checkServiceability(body: any) {
-    const { origin, destination } = body;
-
-    const pipline = [
+  async checkServiceability(body) {
+    let { origin, destination } = body;
+    origin = Number(origin);
+    destination = Number(destination);
+    const pipeline = [
+      { $match: { pincode: { $in: [origin, destination] } } },
       {
-        $facet: {
-          originGrouped: [
-            { $match: { pincode: Number(origin) } },
-            {
-              $lookup: {
-                from: "delivery_partners",
-                localField: "deliveryPartnerId",
-                foreignField: "_id",
-                as: "partner",
-              },
-            },
-            { $unwind: { path: "$partner", preserveNullAndEmptyArrays: true } },
-            {
-              $group: {
-                _id: "$partner.name",
-                originLocations: {
-                  $push: {
-                    _id: "$_id",
-                    pincode: "$pincode",
-                    cityName: "$cityName",
-                    state: "$state",
-                    deliveryPartnerId: "$deliveryPartnerId",
-                  },
-                },
-              },
-            },
-          ],
-          destinationGrouped: [
-            { $match: { pincode: Number(destination)} },
-            {
-              $lookup: {
-                from: "delivery_partners",
-                localField: "deliveryPartnerId",
-                foreignField: "_id",
-                as: "partner",
-              },
-            },
-            { $unwind: { path: "$partner", preserveNullAndEmptyArrays: true } },
-            {
-              $group: {
-                _id: "$partner.name",
-                destinationLocations: {
-                  $push: {
-                    _id: "$_id",
-                    pincode: "$pincode",
-                    cityName: "$cityName",
-                    state: "$state",
-                    deliveryPartnerId: "$deliveryPartnerId",
-                  },
-                },
-              },
-            },
-          ],
+        $lookup: {
+          from: "delivery_partners",
+          localField: "deliveryPartnerId",
+          foreignField: "_id",
+          as: "partner",
         },
       },
-
+      { $unwind: { path: "$partner", preserveNullAndEmptyArrays: true } },
       {
         $project: {
-          merged: {
-            $map: {
-              input: {
-                $setUnion: [
-                  { $map: { input: "$originGrouped", as: "o", in: "$$o._id" } },
-                  {
-                    $map: {
-                      input: "$destinationGrouped",
-                      as: "d",
-                      in: "$$d._id",
-                    },
-                  },
-                ],
-              },
-              as: "partnerName",
-              in: {
-                partnerName: "$$partnerName",
-                originLocations: {
-                  $let: {
-                    vars: {
-                      origin: {
-                        $arrayElemAt: [
-                          {
-                            $filter: {
-                              input: "$originGrouped",
-                              cond: { $eq: ["$$this._id", "$$partnerName"] },
-                            },
-                          },
-                          0,
-                        ],
-                      },
-                    },
-                    in: { $ifNull: ["$$origin.originLocations", []] },
-                  },
-                },
-                destinationLocations: {
-                  $let: {
-                    vars: {
-                      dest: {
-                        $arrayElemAt: [
-                          {
-                            $filter: {
-                              input: "$destinationGrouped",
-                              cond: { $eq: ["$$this._id", "$$partnerName"] },
-                            },
-                          },
-                          0,
-                        ],
-                      },
-                    },
-                    in: { $ifNull: ["$$dest.destinationLocations", []] },
-                  },
-                },
-              },
+          partnerName: "$partner.name",
+          pincode: 1,
+          cityName: 1,
+          state: 1,
+          deliveryPartnerId: 1,
+        },
+      },
+      {
+        $group: {
+          _id: "$partnerName",
+          locations: {
+            $push: {
+              _id: "$_id",
+              pincode: "$pincode",
+              cityName: "$cityName",
+              state: "$state",
+              deliveryPartnerId: "$deliveryPartnerId",
             },
           },
         },
       },
-
       {
-        $replaceRoot: {
-          newRoot: {
-            $arrayToObject: {
-              $map: {
-                input: "$merged",
-                as: "m",
-                in: {
-                  k: "$$m.partnerName",
-                  v: {
-                    originLocations: "$$m.originLocations",
-                    destinationLocations: "$$m.destinationLocations",
-                  },
-                },
-              },
+        $project: {
+          partnerName: "$_id",
+          originLocationsArr: {
+            $filter: {
+              input: "$locations",
+              as: "loc",
+              cond: { $eq: ["$$loc.pincode", origin] },
             },
           },
+          destinationLocationsArr: {
+            $filter: {
+              input: "$locations",
+              as: "loc",
+              cond: { $eq: ["$$loc.pincode", destination] },
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          partnerName: 1,
+          originLocation: { $arrayElemAt: ["$originLocationsArr", 0] },
+          destinationLocation: {
+            $arrayElemAt: ["$destinationLocationsArr", 0],
+          },
+        },
+      },
+      {
+        $match: {
+          originLocation: { $exists: true, $ne: null },
+          destinationLocation: { $exists: true, $ne: null },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          partnerName: 1,
+          originLocations: [{ $ifNull: ["$originLocation", "$$REMOVE"] }],
+          destinationLocations: [
+            { $ifNull: ["$destinationLocation", "$$REMOVE"] },
+          ],
         },
       },
     ];
-
-    const partnerDetails = await this.partnerLocationService.aggregate(pipline);
-    const availablePartners = await this.buildZoneAggregationPipeline(
-      partnerDetails[0],
-    );
-    return {
-      origin,
-      destination,
-      availablePartners,
-    };
+    const result = await this.partnerLocationService.aggregate(pipeline);
+    const currentISOTime11 = new Date().toISOString();
+    const partnerDetails = result.reduce((acc, p) => {
+      acc[p.partnerName] = {
+        originLocations: p.originLocations,
+        destinationLocations: p.destinationLocations,
+      };
+      return acc;
+    }, {});
+    const availablePartners =
+      await this.buildZoneAggregationPipeline(partnerDetails);
+    return { origin, destination, availablePartners };
   }
 
-  async buildZoneAggregationPipeline(partnerDetails) {
-    const pipelines: any = {};
-    const pipelines2: any = {};
 
-    pipelines.delhivery = [
-      {
-        $match: {
-          deliveryPartnerId: new Types.ObjectId(
-            partnerDetails.Delhivery.originLocations[0].deliveryPartnerId,
-          ),
-          originCity: new RegExp(
-            `^${partnerDetails.Delhivery.originLocations[0].cityName}$`,
-            "i",
-          ),
-          destinationCity: new RegExp(
-            `^${partnerDetails.Delhivery.destinationLocations[0].cityName}$`,
-            "i",
-          ),
-        },
-      },
-      {
-        $limit: 1,
-      },
-      {
-        $project: {
-          _id: 0,
-          zone: 1,
-        },
-      },
-    ];
 
-    pipelines.shadowfox = [
-      {
-        $match: {
-          deliveryPartnerId: new Types.ObjectId(
-            partnerDetails.Shadowfox.originLocations[0].deliveryPartnerId,
-          ),
-          originCity: new RegExp(
-            `^${partnerDetails.Shadowfox.originLocations[0].cityName}$`,
-            "i",
-          ),
-          destinationCity: new RegExp(
-            `^${partnerDetails.Shadowfox.destinationLocations[0].cityName}$`,
-            "i",
-          ),
-          originState: new RegExp(
-            `^${partnerDetails.Shadowfox.originLocations[0].state}$`,
-            "i",
-          ),
-          destinationState: new RegExp(
-            `^${partnerDetails.Shadowfox.destinationLocations[0].state}$`,
-            "i",
-          ),
-        },
-      },
-
-      {
-        $limit: 1,
-      },
-      {
-        $project: {
-          _id: 0,
-          zone: 1,
-        },
-      },
-    ];
-
-    if (Object.keys(pipelines).length === 0) {
-      return null;
-    }
-
-    const DelhiveryAndShadowfox =
-      await this.zoneMappingService.aggregateCouriers([
-        {
-          $match: {
-            $or: [
-              {
-                originCity:
-                  partnerDetails?.Delhivery?.originLocations?.[0]?.cityName?.toUpperCase(),
-                destinationCity:
-                  partnerDetails?.Delhivery?.destinationLocations?.[0]?.cityName?.toUpperCase(),
-              },
-              {
-                originCity:
-                  partnerDetails?.Shadowfox?.originLocations?.[0]?.cityName?.toUpperCase(),
-                destinationCity:
-                  partnerDetails?.Shadowfox?.destinationLocations?.[0]?.cityName?.toUpperCase(),
-              },
-            ],
+async buildZoneAggregationPipeline(partnerDetails) {
+    try {
+      const pipelines: Record<string, any[]> = {};
+      const get = (obj, path, fallback = null) => {
+        return (
+          path.split(".").reduce((acc, part) => acc?.[part], obj) ?? fallback
+        );
+      };
+      if (
+        partnerDetails?.Delhivery?.originLocations?.length &&
+        partnerDetails?.Delhivery?.destinationLocations?.length
+      ) {
+        pipelines.Delhivery = [
+          {
+            $match: {
+              deliveryPartnerId: new Types.ObjectId(
+                get(
+                  partnerDetails,
+                  "Delhivery.originLocations.0.deliveryPartnerId",
+                ),
+              ),
+              originCity: new RegExp(
+                `^${get(partnerDetails, "Delhivery.originLocations.0.cityName")}$`,
+                "i",
+              ),
+              destinationCity: new RegExp(
+                `^${get(partnerDetails, "Delhivery.destinationLocations.0.cityName")}$`,
+                "i",
+              ),
+            },
           },
-        },
-        {
-          $facet: pipelines,
-        },
-      ]);
-
-
-        const aggregatedData = await this.zoneMappingService.aggregateCouriers([
-      {
-        $match: {
-          pincode: {
-            $in: [
-              partnerDetails.Ekart.originLocations[0]._id,
-              partnerDetails.Ekart.destinationLocations[0]._id,
-              partnerDetails.Xpressbees.originLocations[0]._id,
-              partnerDetails.Xpressbees.destinationLocations[0]._id,
-              
-            ],
-          },
-        },
-      },
-    ]);
-
-    const deliveryPartnersWithPincode = ["Ekart", "Xpressbees"];
-
-    for (const partner of deliveryPartnersWithPincode) {
-      const partnerInfo = partnerDetails[partner];
-
-      const originLocation = partnerInfo.originLocations[0];
-      const destinationLocation = partnerInfo.destinationLocations[0];
-
-      const originZone = aggregatedData.find(
-        (zone) =>
-          zone.pincode.toString() === originLocation._id.toString() &&
-          zone.deliveryPartnerId.toString() ===
-            originLocation.deliveryPartnerId.toString() &&
-          zone.originCity.toLowerCase() ===
-            originLocation.cityName.toLowerCase(),
-      );
-
-      const destinationZone = aggregatedData.find(
-        (zone) =>
-          zone.pincode.toString() === destinationLocation._id.toString() &&
-          zone.deliveryPartnerId.toString() ===
-            destinationLocation.deliveryPartnerId.toString() &&
-          zone.originCity.toLowerCase() ===
-            destinationLocation.cityName.toLowerCase(),
-      );
-
-      const zone = this.determineZone(originZone, destinationZone);
-
-      DelhiveryAndShadowfox[0][partner] = [{ zone }];
-
+          { $limit: 1 },
+          { $project: { _id: 0, zone: 1 } },
+        ];
       }
-
-    return DelhiveryAndShadowfox;
+      if (
+        partnerDetails?.Shadowfox?.originLocations?.length &&
+        partnerDetails?.Shadowfox?.destinationLocations?.length
+      ) {
+        pipelines.Shadowfox = [
+          {
+            $match: {
+              deliveryPartnerId: new Types.ObjectId(
+                get(
+                  partnerDetails,
+                  "Shadowfox.originLocations.0.deliveryPartnerId",
+                ),
+              ),
+              originCity: new RegExp(
+                `^${get(partnerDetails, "Shadowfox.originLocations.0.cityName")}$`,
+                "i",
+              ),
+              destinationCity: new RegExp(
+                `^${get(partnerDetails, "Shadowfox.destinationLocations.0.cityName")}$`,
+                "i",
+              ),
+              originState: new RegExp(
+                `^${get(partnerDetails, "Shadowfox.originLocations.0.state")}$`,
+                "i",
+              ),
+              destinationState: new RegExp(
+                `^${get(partnerDetails, "Shadowfox.destinationLocations.0.state")}$`,
+                "i",
+              ),
+            },
+          },
+          { $limit: 1 },
+          { $project: { _id: 0, zone: 1 } },
+        ];
+      }
+      if (Object.keys(pipelines).length === 0) {
+        console.warn("No valid partners found for zone pipeline.");
+        return null;
+      }
+      const DelhiveryAndShadowfox =
+        await this.zoneMappingService.aggregateCouriers([
+          {
+            $match: {
+              $or: [
+                ...(partnerDetails?.Delhivery
+                  ? [
+                      {
+                        originCity:
+                          partnerDetails.Delhivery.originLocations[0].cityName.toUpperCase(),
+                        destinationCity:
+                          partnerDetails.Delhivery.destinationLocations[0].cityName.toUpperCase(),
+                      },
+                    ]
+                  : []),
+                ...(partnerDetails?.Shadowfox
+                  ? [
+                      {
+                        originCity:
+                          partnerDetails.Shadowfox.originLocations[0].cityName.toUpperCase(),
+                        destinationCity:
+                          partnerDetails.Shadowfox.destinationLocations[0].cityName.toUpperCase(),
+                      },
+                    ]
+                  : []),
+              ],
+            },
+          },
+          { $facet: pipelines },
+        ]);
+      const pincodePartners = ["Ekart", "Xpressbees"];
+      const pincodeIds: any[] = [];
+      for (const partner of pincodePartners) {
+        const originId = get(
+          partnerDetails,
+          `${partner}.originLocations.0._id`,
+        );
+        const destId = get(
+          partnerDetails,
+          `${partner}.destinationLocations.0._id`,
+        );
+        if (originId) pincodeIds.push(originId);
+        if (destId) pincodeIds.push(destId);
+      }
+      const aggregatedData = pincodeIds.length
+        ? await this.zoneMappingService.aggregateCouriers([
+            { $match: { pincode: { $in: pincodeIds } } },
+          ])
+        : [];
+      for (const partner of pincodePartners) {
+        const partnerInfo = partnerDetails?.[partner];
+        if (
+          !partnerInfo?.originLocations?.length ||
+          !partnerInfo?.destinationLocations?.length
+        )
+          continue;
+        const originLocation = partnerInfo.originLocations[0];
+        const destinationLocation = partnerInfo.destinationLocations[0];
+        const originZone = aggregatedData.find(
+          (z) =>
+            z.pincode.toString() === originLocation._id.toString() &&
+            z.deliveryPartnerId.toString() ===
+              originLocation.deliveryPartnerId.toString(),
+        );
+        const destinationZone = aggregatedData.find(
+          (z) =>
+            z.pincode.toString() === destinationLocation._id.toString() &&
+            z.deliveryPartnerId.toString() ===
+              destinationLocation.deliveryPartnerId.toString(),
+        );
+        const zone = this.determineZone(originZone, destinationZone);
+        if (!DelhiveryAndShadowfox[0]) DelhiveryAndShadowfox[0] = {};
+        DelhiveryAndShadowfox[0][partner] = [{ zone }];
+      }
+      return DelhiveryAndShadowfox;
+    } catch (err) {
+      console.error(" Error in buildZoneAggregationPipeline:", err.message);
+      return {
+        error: true,
+        message: "Failed to build zone aggregation pipeline",
+        details: err.message,
+      };
+    }
   }
-
   private determineZone(originZone: any, destinationZone: any): string {
     if (!originZone || !destinationZone) return "NA";
-
-    if (
-      originZone.originCity.toLowerCase() ===
-      destinationZone.originCity.toLowerCase()
-    )
-      return "Zone A";
-    if (
-      originZone.region.toLowerCase() === destinationZone.region.toLowerCase()
-    )
-      return "Zone B";
-    if (
-      originZone.metro.toLowerCase() === "yes" &&
-      destinationZone.metro.toLowerCase() === "yes"
-    )
-      return "Zone C";
+    const oCity = originZone.originCity?.toLowerCase?.() || "";
+    const dCity = destinationZone.originCity?.toLowerCase?.() || "";
+    const oRegion = originZone.region?.toLowerCase?.() || "";
+    const dRegion = destinationZone.region?.toLowerCase?.() || "";
+    const oMetro = originZone.metro?.toLowerCase?.() || "";
+    const dMetro = destinationZone.metro?.toLowerCase?.() || "";
+    if (oCity === dCity) return "Zone A";
+    if (oRegion === dRegion) return "Zone B";
+    if (oMetro === "yes" && dMetro === "yes") return "Zone C";
     const specialRegions = [
-      "North East and Special states",
-      "North East and J&K",
+      "north east and special states",
+      "north east and j&k",
     ];
-    if (
-      specialRegions.includes(originZone.region) ||
-      specialRegions.includes(destinationZone.region)
-    )
+    if (specialRegions.includes(oRegion) || specialRegions.includes(dRegion))
       return "Zone E";
-
     return "Zone D";
   }
 }
